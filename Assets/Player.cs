b9398b64efb50a11c52f656b2c8e1bb46174f762
RabbitLearning <82035001+RabbitLearning@users.noreply.github.com>
2021-04-06 06:13:32 -0700

    Part1
    This is the first step of animator controller.

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator LukeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LukeAnim.SetBool("Run", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
